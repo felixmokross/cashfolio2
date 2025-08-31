77b6876920e3bba2d048126dfb9787dae02bbcdb
Felix Mokross <felixmokross@gmail.com>
2025-08-31 19:55:24 +0200

    Enable creating new accounts

--- apps/cashfolio-app/app/routes/accounts.tsx
+++ apps/cashfolio-app/app/routes/accounts.tsx
@@ -1,6 +1,5 @@
-import { PrismaClient } from "@prisma/client";
 import { useState } from "react";
-import { useLoaderData } from "react-router";
+import { Form, useLoaderData, type ActionFunctionArgs } from "react-router";
 import { Button } from "~/catalyst/button";
 import {
   Dialog,
@@ -19,16 +18,27 @@ import {
 import { Input } from "~/catalyst/input";
 import { Radio, RadioField, RadioGroup } from "~/catalyst/radio";
 import { Select } from "~/catalyst/select";
+import { prisma } from "~/prisma.server";
+import slugify from "slugify";
 
 export async function loader() {
-  const prisma = new PrismaClient();
-  try {
-    await prisma.$connect();
+  return await prisma.account.findMany();
+}
 
-    return await prisma.account.findMany();
-  } finally {
-    prisma.$disconnect();
+export async function action({ request }: ActionFunctionArgs) {
+  if (request.method !== "POST") return new Response(null, { status: 405 });
+
+  const form = await request.formData();
+  const name = form.get("name");
+  if (typeof name !== "string") {
+    return new Response(null, { status: 400 });
   }
+
+  await prisma.account.create({
+    data: { name: name, slug: slugify(name, { lower: true }) },
+  });
+
+  return new Response(null, { status: 201 });
 }
 
 export default function Accounts() {
@@ -38,65 +48,67 @@ export default function Accounts() {
     <div>
       <Button onClick={() => setIsOpen(true)}>New Account</Button>
       <Dialog open={isOpen} onClose={setIsOpen} size="3xl">
-        <DialogTitle>New Account</DialogTitle>
-        <DialogDescription>
-          The refund will be reflected in the customer’s bank account 2 to 3
-          business days after processing.
-        </DialogDescription>
-        <DialogBody>
-          <Fieldset>
-            <FieldGroup>
-              <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 sm:gap-4">
-                <Field>
-                  <Label>Name</Label>
-                  <Input name="name" />
-                </Field>
-                <Field>
-                  <Label>Group</Label>
-                  <Select name="group"></Select>
-                </Field>
-              </div>
-              <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 sm:gap-4">
-                <Field>
-                  <Label>Unit</Label>
-                  <RadioGroup name="resale" defaultValue="permit">
-                    <RadioField>
-                      <Radio value="permit" />
-                      <Label>Currency</Label>
-                      <Description>
-                        Customers can resell or transfer their tickets if they
-                        can’t make it to the event.
-                      </Description>
-                    </RadioField>
-                    <RadioField>
-                      <Radio value="forbid" />
-                      <Label>Security</Label>
-                      <Description>
-                        Tickets cannot be resold or transferred to another
-                        person.
-                      </Description>
-                    </RadioField>
-                  </RadioGroup>
-                </Field>
-                <Field>
-                  <Label>Currency</Label>
-                  <Select name="group">
-                    <option value="active">CHF</option>
-                    <option value="paused">EUR</option>
-                    <option value="delayed">USD</option>
-                    <option value="canceled">DKK</option>
-                  </Select>
-                </Field>
-              </div>
-            </FieldGroup>
-          </Fieldset>
-        </DialogBody>
-        <DialogActions>
-          <Button plain onClick={() => setIsOpen(false)}>
-            Cancel
-          </Button>
-          <Button onClick={() => setIsOpen(false)}>Create</Button>
-        </DialogActions>
+        <Form method="POST" onSubmit={() => setIsOpen(false)}>
+          <DialogTitle>New Account</DialogTitle>
+          <DialogDescription>
+            The refund will be reflected in the customer’s bank account 2 to 3
+            business days after processing.
+          </DialogDescription>
+          <DialogBody>
+            <Fieldset>
+              <FieldGroup>
+                <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 sm:gap-4">
+                  <Field>
+                    <Label>Name</Label>
+                    <Input name="name" />
+                  </Field>
+                  <Field>
+                    <Label>Group</Label>
+                    <Select name="group"></Select>
+                  </Field>
+                </div>
+                <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 sm:gap-4">
+                  <Field>
+                    <Label>Unit</Label>
+                    <RadioGroup name="resale" defaultValue="permit">
+                      <RadioField>
+                        <Radio value="permit" />
+                        <Label>Currency</Label>
+                        <Description>
+                          Customers can resell or transfer their tickets if they
+                          can’t make it to the event.
+                        </Description>
+                      </RadioField>
+                      <RadioField>
+                        <Radio value="forbid" />
+                        <Label>Security</Label>
+                        <Description>
+                          Tickets cannot be resold or transferred to another
+                          person.
+                        </Description>
+                      </RadioField>
+                    </RadioGroup>
+                  </Field>
+                  <Field>
+                    <Label>Currency</Label>
+                    <Select name="group">
+                      <option value="active">CHF</option>
+                      <option value="paused">EUR</option>
+                      <option value="delayed">USD</option>
+                      <option value="canceled">DKK</option>
+                    </Select>
+                  </Field>
+                </div>
+              </FieldGroup>
+            </Fieldset>
+          </DialogBody>
+          <DialogActions>
+            <Button plain onClick={() => setIsOpen(false)}>
+              Cancel
+            </Button>
+            <Button type="submit">Create</Button>
+          </DialogActions>
+        </Form>
       </Dialog>
       <ul>
         {accounts.map((a) => (
