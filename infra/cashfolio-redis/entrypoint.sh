ccef45b3aad64c988831c68929eebb271887216d
Felix Mokross <felixmokross@gmail.com>
2025-10-24 20:36:42 +0200

    Actually run the Dockerfile CMD

--- infra/cashfolio-redis/entrypoint.sh
+++ infra/cashfolio-redis/entrypoint.sh
@@ -13,3 +13,6 @@ user default off
 user admin on >$REDIS_ADMIN_PASS ~* +@all
 user cashfolio-app on >$REDIS_APP_PASS ~* +@read +@write +@set +@keyspace -@dangerous
 EOF
+
+echo "🚀 Starting: $@"
+exec "$@"
