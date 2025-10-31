8ca914c7f7c5d58e21af76ba7071e73862db83cd
Felix Mokross <felixmokross@gmail.com>
2025-10-31 22:53:32 +0100

    infra: Fix restore-local CLI command

--- tools/cli/src/commands/restore-local.ts
+++ tools/cli/src/commands/restore-local.ts
@@ -3,10 +3,18 @@ import { exec } from "../shared";
 import { createClient } from "redis";
 
 program.command("restore-local").action(async () => {
-  console.log("Restoring local database from backup…");
-
+  console.log("Dropping local database…");
   await exec(
-    `pg_restore --create --clean --no-owner --no-privileges -d "${process.env.RESTORE_DB_URL}" ./backup.dump`,
+    `psql -d "${process.env.RESTORE_DB_URL}" -c "DROP DATABASE IF EXISTS ${process.env.RESTORE_DB_NAME}"`,
+  );
+  console.log("Creating local database…");
+  await exec(
+    `psql -d "${process.env.RESTORE_DB_URL}" -c "CREATE DATABASE ${process.env.RESTORE_DB_NAME}"`,
+  );
+
+  console.log("Restoring local database from backup…");
+  await exec(
+    `pg_restore --no-owner --no-privileges -d "${process.env.RESTORE_DB_URL}/${process.env.RESTORE_DB_NAME}" ./backup.dump`,
   );
 
   console.log("Purging local Redis…");
